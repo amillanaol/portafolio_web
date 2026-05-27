5d4252a428ba09bc8ff0a582b38af5fe73f6f60c
amillanaol <alexis.millanao@hotmail.com>
2026-05-26 22:49:54 -0400

    fix: resolve linting errors after eslint migration

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
 import nuxtPlugin from 'eslint-plugin-nuxt'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   js.configs.recommended,
@@ -9,6 +11,22 @@ export default [
   ...pluginVue.configs['flat/essential'],
   {
     files: ['**/*.ts', '**/*.vue'],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        useAbout: 'readonly',
+        useProjects: 'readonly',
+        useServices: 'readonly',
+        useStack: 'readonly',
+        useScrollReveal: 'readonly',
+        useHead: 'readonly',
+        ref: 'readonly',
+        onMounted: 'readonly',
+        onUnmounted: 'readonly'
+      }
+    },
     plugins: {
       nuxt: nuxtPlugin
     },
