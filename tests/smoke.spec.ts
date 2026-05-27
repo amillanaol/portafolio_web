647817854b1d68122113e70648b7b971bbd95aaf
amillanaol <alexis.millanao@hotmail.com>
2026-05-27 19:22:32 -0400

    chore: clean repo

--- tests/smoke.spec.ts
+++
@@ -1,8 +0,0 @@
-﻿import { test, expect } from '@playwright/test'
-
-const URL = process.env.TEST_URL || 'http://localhost:4566/landing-page/index.html'
-
-test('index loads', async ({ page }) => { 
-  await page.goto(URL, { waitUntil: 'networkidle' })
-  await expect(page.locator('#__nuxt')).not.toBeEmpty()
-})
