3955bda795e6622a2711e9e5da72595c090d0067
amillanaol <alexis.millanao@hotmail.com>
2026-05-27 03:33:46 -0400

    fix(config): agregar límite de fallas en configuración
    - Se agrega la propiedad maxFailures para limitar el número de fallas permitidas en las pruebas
    - Esto ayuda a detener las pruebas automáticamente cuando se supera el límite de fallas

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './tests',
   reporter: 'list',
+  maxFailures: 1,
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
