b109c84b997b8eec79d3b06c7ef5cb7b829f5a1f
amillanaol <amillanaol@users.noreply.github.com>
2026-05-27 02:03:17 +0000

    deploy: e3c6bc1bbd9de5be9bb51dca43aefbf319e5a9e4

---
+++ _nuxt/OiQGW-vv.js
@@ -0,0 +1,3 @@
+const e={name:"Alexis Millanao",title:"Desarrollador de Software Full Stack",summary:`Desarrollo soluciones web, automatización e integraciones para negocios, con un enfoque funcional, mantenible y escalable.
+
+También apoyo procesos operativos y digitales que requieren orden, continuidad y ejecución confiable.`,github:"https://github.com/amillanaol",buymeacoffee:"https://www.buymeacoffee.com/amillanaol",patreon:"https://www.patreon.com/cw/amillanaol"};function a(){return{about:e}}export{a as u};
