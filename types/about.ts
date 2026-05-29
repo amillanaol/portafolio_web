f3d6555a4bcb89f6c23bacf6e264ad2786bca019
amillanaol <alexis.millanao@hotmail.com>
2026-05-28 22:42:03 -0400

    feat(types): agregar sección sobre mí
    - Crear interfaz AboutSection para representar secciones en la página de sobre mí
    - Agregar campo sections a la interfaz AboutData para almacenar secciones

--- types/about.ts
+++ types/about.ts
@@ -1,7 +1,13 @@
+export interface AboutSection {
+  title: string
+  items: string[]
+}
+
 export interface AboutData {
   name: string
   title: string
   summary: string
+  sections: AboutSection[]
   github: string
   buymeacoffee: string
   patreon: string
